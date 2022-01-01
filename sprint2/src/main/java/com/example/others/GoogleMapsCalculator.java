477a905d01a274a6568cc3eb9fa3ac9a5df119d9
Shama352 <65794387+Shama352@users.noreply.github.com>
2022-01-01 10:08:24 +0200

    API

---
+++ sprint2/src/main/java/com/example/others/GoogleMapsCalculator.java
@@ -0,0 +1,5 @@
+package com.example.others;
+
+public class GoogleMapsCalculator implements IDistanceCalculator {
+    
+}
---
+++ sprint2/src/main/java/com/example/others/HarvesineCalculator.java
@@ -0,0 +1,5 @@
+package com.example.others;
+
+public class HarvesineCalculator {
+    
+}
