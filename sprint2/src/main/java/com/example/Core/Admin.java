c09fa83d0b16dd0871eddf9ff42df032b2b7382d
Shama352 <65794387+Shama352@users.noreply.github.com>
2022-05-27 16:14:43 +0200

    v1

--- sprint2/src/main/java/com/example/Core/Admin.java
+++ sprint2/src/main/java/com/example/Core/Admin.java
@@ -1,28 +1,24 @@
 package com.example.Core;
-import java.util.ArrayList;
-import java.util.List;
 
 import com.example.Persistence.ListDriverPersistence;
 
-public class Admin extends ListDriverPersistence  {
+public class Admin extends ListDriverPersistence {
 
     // public boolean getPendingList()
     // {
-    //     boolean flag = false;
-    //     for (int i = 0 ;i<drivers.size();i++)
-    //     {
-    //         if(drivers.get(i).verified == 0)
-    //         {
-    //             flag = true;
-    //             System.out.println(i+1 + " - " + drivers.get(i).username + " " );
+    // boolean flag = false;
+    // for (int i = 0 ;i<drivers.size();i++)
+    // {
+    // if(drivers.get(i).verified == 0)
+    // {
+    // flag = true;
+    // System.out.println(i+1 + " - " + drivers.get(i).username + " " );
 
-    //         }
-
-    //     }
-    //     return flag;
     // }
 
-
+    // }
+    // return flag;
+    // }
+    // comment
 
 }
-
